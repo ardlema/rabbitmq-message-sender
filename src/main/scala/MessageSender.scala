6f97905b8b81f62fdd841e2a00d0f98f119e223f
Alberto <ardlema@gmail.com>
2013-12-20 13:53:24 +0100

    Connection creation

--- src/main/scala/MessageSender.scala
+++ src/main/scala/MessageSender.scala
@@ -1,7 +1,21 @@
 package ardlema.rabbitmqmessagesender
 
+import com.rabbitmq.client.ConnectionFactory
+
 object MessageSender {
   def main(args: Array[String]) {
-    println("Hello World!!!")
+    val userName = "guest"
+    val password = "guest"
+    val hostName = "localhost"
+    val portNumber = 5672
+    val factory = new ConnectionFactory()
+    factory.setUsername(userName)
+    factory.setPassword(password)
+    //factory.setVirtualHost(virtualHost)
+    factory.setHost(hostName)
+    factory.setPort(portNumber)
+    val conn = factory.newConnection()
+
+    println("Connection created!!!!!!")
   }
 }
